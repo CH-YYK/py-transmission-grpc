f7732af23fa0f6cf6c1b13325521c2e199e248b7
ch-yyk <yangyikang1995@126.com>
2019-12-31 14:50:19 -0800

    update README and test

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 install:
 	pip3 install -r requirements.txt
 
+build: docker/Dockerfile
+	docker build --no-cache -t transmission_grpc:latest ./
+
 run: transmission_call_server.py
-	python3 transmission_call_server.py
+	docker run -p 5051:5051 transmission_grpc:latest
+
+test: transmission_call_client.py
+	python3 transmission_call_client.py --host localhost --port 5051
--- transmission_call_client.py
+++ transmission_call_client.py
@@ -1,3 +1,4 @@
+import argparse
 import grpc
 
 import transmission_call_pb2 as pb2
@@ -5,6 +6,22 @@ import transmission_call_pb2_grpc as pb2_grpc
 
 _host = "192.168.1.6"
 _port = 5051
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host",
+                        help="The host of grpc server")
+
+    parser.add_argument("-p", "--port",
+                        help="The port listened by the server",
+                        type=int)
+
+    parser.add_argument("-t", "--torrent_url")
+
+    parser.add_argument("-i", "--id")
+
+    return parser
 
 def make_TorrentID(torrent_id):
     """make TorrentID object"""
@@ -35,6 +52,7 @@ def transmission_send_torrent(stub, torrent_url):
 def run():
     """run the stub"""
     with grpc.insecure_channel('%s:%d' % (_host, _port)) as channel:
+        print("---- start communicating with %s at %d ---- " % (_host, _port))
         stub = pb2_grpc.TransmissionCallStub(channel)
 
         print("---- get torrent ----")
@@ -44,4 +62,11 @@ def run():
 
 
 if __name__ == '__main__':
+    cmdlines = make_parser().parse_args()
+
+    if cmdlines.host:
+        _host = cmdlines.host
+    if cmdlines.port:
+        _port = cmdlines.port
+
     run()
